574438ad584309775600eb5365e76ca650925c54
luis enrique <20240698@itla.edu.do>
2024-09-12 00:08:12 -0700

    ejercicios 1

---
+++ programas capitulo 1/programa1.1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main (void)
+{
+printf("mi primer programa de C");
+return 0;
+}
+/*profesor pegue el programa como estaba en el libro para comprobar si yo lo escribi mal,
+ aun asi no me funciono asi que cambie la funcion por int para que funcionase(lo hice honestamente)/*
